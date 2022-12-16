817d56bf988b82e2d65198e8c646da36f1b22536
foggy_hedgehog <foggy80hedgehog@gmail.com>
2022-12-16 12:01:41 +0300

    Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
